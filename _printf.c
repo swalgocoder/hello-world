8d80528fa8379fff571e2d819254c3d72c3eb29a
swalgocoder <gary.d.gao@gmail.com>
2016-11-21 20:27:04 -0800

    Create _printf.c

---
+++ _printf.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _printf - replicate the printf function
+ * @format: character string
+ *
+ * Return: number of characters printed (not including '\0')
+ */
+int _printf(const char *format, ...)
+{
+	va_list ptr_args;
+	char *buffer;
+	int i, n;
+	print_format switchf[] = {
+		{"i", my_printnum},
+		{"c", my_printchar},
+		{"s", my_printstr},
+		{"d", my_printnum},
+	};
+
+	va_start(ptr_args, format);
+	/* looping format*/
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		n = 0;
+		while (format[i] != '%')
+		{
+			buffer[n] = format[i];
+			if (format[i] == '%')
+			{
+				i++;
+				buffer = switchf[n].f(buffer, ptr_args);
+			}
+			n++;
+		}
+	}
+	my_printstring(buffer);
+	va_end(ptr_args);
+	return (i);
+}
